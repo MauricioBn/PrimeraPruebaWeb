d4c895cad5fbcf1eec5836d4c5ec22b56cb6fe30
MauricioBn <josuej2603@gmail.com>
2026-03-19 18:12:35 -0500

    Cambios en el tipo de letra, se añadió wsp/gmail

--- scripts/main.js
+++ scripts/main.js
@@ -11,37 +11,4 @@ if (hora >= 0 && hora <=5) {
 }
 
 
-let miImage = document.querySelector("img");
-miImage.onclick = function () {
-  let miSrc = miImage.getAttribute("src");
-  if (miSrc === "images/tarjeta-partefrontal.png") {
-    miImage.setAttribute("src", "images/tarjeta-parteposterior.png");
-  } else {
-    miImage.setAttribute("src", "images/tarjeta-partefrontal.png");
-  }
-};
-
-let Boton =document.querySelector("button")
-let TituloPag = document.querySelector("#saludo")
-
-function estableceNombreUsuario() {
-  let miNombre = prompt("Por favor, ingresa tu nombre.");
-  if(!miNombre) {
-    estableceNombreUsuario();
-  } else {
-    localStorage.setItem("nombre", miNombre)
-    TituloPag.innerHTML = "Bienvenido, " + miNombre;
-  }
-}
-
-if (!localStorage.getItem("nombre")) {
-  estableceNombreUsuario();
-} else {
-  let nombreAlmacenado = localStorage.getItem("nombre");
-  TituloPag.textContent = "Bienvenido, " + nombreAlmacenado;
-}
-
-Boton.onclick = function () {
-  estableceNombreUsuario();
-};
 
